f68b42d5dbb4d12aa415fcc122243bc2f826dca3
G <frolovgeorgiy@gmail.com>
2022-03-06 15:12:56 +0300

    MVP update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-let data = localStorage.solverBookData || [{name: 'fuck', link:'http://google.com'}, {name: 'fucking', link:'google.com'}, {name: 'sex', link:'google.com'}];
+let data = JSON.parse(localStorage.getItem('solverBookData')) || [{name: 'fuck', link:'http://google.com'}, {name: 'fucking', link:'google.com'}, {name: 'sex', link:'google.com'}];
 
+let thisBody = document.querySelector('body');
+let nameEnterInput = document.querySelector('input.bookmarkName');
+let addButton = document.querySelector('.addButton');
+let nameEnterWrapper = document.querySelector('.nameEnterWrapper');
 let searchElement = document.querySelector('input.searchBookmark');
 let bookmarksListElement = document.querySelector('.taskList');
 console.log(bookmarksListElement);
 function createTask(name, link) {
     let element = document.createElement("a")
     element.href = link;
+    element.title = "Open the bookmark";
     element.target = "_blank";
     element.innerHTML = name;
     return element;
@@ -14,7 +19,7 @@ function createTask(name, link) {
 function renderBookList() {
     clearBookList();
     text = this.value || '';
-    console.log(text);
+    text = text.toLowerCase();
     for (let bookmark of data) {
         if (bookmark.name.includes(text)) {
             bookmarksListElement.prepend(createTask(bookmark.name, bookmark.link));
@@ -27,6 +32,50 @@ function clearBookList() {
     bookmarksListElement.innerHTML = '';
 }
 
+function openAddingMenu() {
+    nameEnterWrapper.classList.add('active');
+    nameEnterInput.focus();
+    nameEnterWrapper.addEventListener('keydown', keyCheck);
+}
+
+function closeAddingMenu() {
+    nameEnterWrapper.removeEventListener('keydown', keyCheck);
+    nameEnterWrapper.classList.remove('active');
+    searchElement.focus();
+}
+
+function addingBookmark() {
+    data.push({
+        name: nameEnterInput.value,
+        link: window.location.href
+    });
+    
+    
+    searchElement.value = '';
+    closeAddingMenu();
+    renderBookList();
+    nameEnterInput.value = '';
+    localStorageAdding();
+}
+
+function keyCheck(e) {
+    if (e.key === "Escape") {
+        closeAddingMenu();
+    }
+    if (e.key === "Enter" && nameEnterInput.value!='') {
+        addingBookmark();
+    }
+}
+
+function localStorageAdding() {
+    localStorage.setItem('solverBookData', JSON.stringify(data)); 
+}
+
+
 renderBookList();
-searchElement.addEventListener('change', renderBookList);
+searchElement.addEventListener('keyup', renderBookList);
+addButton.addEventListener('click', openAddingMenu);
+searchElement.focus();
+
+
 
